39657f66e5e34f9b27d1890c5d782a3f593b57fd
Konrad Strachan <konrad.strachan@gmail.com>
2025-07-02 11:06:08 +0100

    helpers

---
+++ scripts/build_and_install.sh
@@ -0,0 +1,2 @@
+gnome-extensions pack
+gnome-extensions install next-up@konradstrachan.github.com.shell-extension.zip
---
+++ scripts/createsym.sh
@@ -0,0 +1,5 @@
+cd ../
+rm -rf ~/.local/share/gnome-shell/extensions/next-up@konradstrachan.github.com/
+#ln -s gnomecryptoticker ~/.local/share/gnome-shell/extensions/next-up@konradstrachan.github.com/
+#mkdir ~/.local/share/gnome-shell/extensions/next-up@konradstrachangithub.com/
+#cp -r gnome-next-up-gnome48/* ~/.local/share/gnome-shell/extensions/next-up@konradstrachan.github.com/
